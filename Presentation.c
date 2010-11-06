c01dd6cb86722169ac583ee607b988ee70129639
Dan <dantastic@shaw.ca>
2010-11-06 15:29:05 -0700

    display unsupported tag on unrecognized rfid token

--- Presentation.c
+++ Presentation.c
@@ -283,55 +283,49 @@ VOID ProcessPacket(HWND hWnd, CHAR* pcPacket, DWORD dwLength){
 	DWORD j, i;
 	pwd = (PWNDDATA) GetWindowLongPtr(hWnd, 0);
 
-	/*if(DetectLRCError(hWnd, pcPacket, dwLength)){
+	if(DetectLRCError(hWnd, pcPacket, dwLength)){
 		DISPLAY_ERROR("Error in RFID Packet");
-		//display error packet error
-	}*/
+	}
 	
 	switch(pcPacket[7]){
 		case 0x04:
-			if(pcPacket[8] == 0x00 && pcPacket[9] == 0x00){
-				(pcToken , "ISO 15693");
-				dwTokenLength = strlen(pcToken);
-				dwDataLength = 8;
-				j = (dwLength - 3);
-				for(i = 0; i < dwDataLength; i++){
-					pcData[i] = pcPacket[j];
-					j--;
-				}
-				//EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
-			} else {
-				//display error unsupported token
+			(pcToken , "ISO 15693");
+			dwTokenLength = strlen(pcToken);
+			dwDataLength = 8;
+			j = (dwLength - 3);
+			for(i = 0; i < dwDataLength; i++){
+				pcData[i] = pcPacket[j];
+				j--;
 			}
+			EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
 			return;
 		case 0x05:
-			if(pcPacket[8] == 0x02 && pcPacket[9] == 0x24){
-				strcpy(pcToken , "TAG-IT HF");
-				dwTokenLength = strlen(pcToken);
-				dwDataLength = 4;
-				for(i = 0, j = (dwLength - 1); i < dwDataLength; i++, j--){
-					pcData[i] = pcPacket[j];
-				}
-				EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
-			} else {
-				//display error unsupported token
+			
+			strcpy(pcToken , "TAG-IT HF");
+			dwTokenLength = strlen(pcToken);
+			dwDataLength = 4;
+			for(i = 0, j = (dwLength - 1); i < dwDataLength; i++, j--){
+				pcData[i] = pcPacket[j];
 			}
+			EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
+			
 			return;
 		case 0x06:
-			if(pcPacket[8] == 0xFE){
-				strcpy(pcToken , "LF R/W");
-				dwTokenLength = strlen(pcToken);
-				dwDataLength = 8;
-				for(i = 0, j = (dwLength - 3); i < dwDataLength; i++, j--){
-					pcData[i] = pcPacket[j];
-				}
-				EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
-			} else {
-				//display error unsupported token
+	
+			strcpy(pcToken , "LF R/W");
+			dwTokenLength = strlen(pcToken);
+			dwDataLength = 8;
+			for(i = 0, j = (dwLength - 3); i < dwDataLength; i++, j--){
+				pcData[i] = pcPacket[j];
 			}
+			EchoTag(hWnd, pcToken, dwTokenLength, pcData, dwDataLength);
+			
 			return;
 		default:
-			//display error unsupported token
+			strcpy(pcToken, "Unsupported Tag");
+			for(i = 0; i < strlen(pcToken); i++){
+				UpdateDisplayBuf(hWnd, pcToken[i]);
+			}
 			return;
 	}
 }
